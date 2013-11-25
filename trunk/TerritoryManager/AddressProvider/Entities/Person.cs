e7d6924ed71b2bce17a543739de2270407376e58
kaczmarek.mhl@gmail.com <kaczmarek.mhl@gmail.com@bd7958f3-32f3-4e9c-53a0-a9d844338e45>
2013-11-25 19:03:29 +0000

    Project Start :)

---
+++ trunk/TerritoryManager/AddressProvider/Entities/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AddressSearch.AdressProvider.Entities
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string StreetAddress { get; set; }
+        public string Locality { get; set; }
+        public string PostCode { get; set; }
+        public string TelephoneNumber { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+    }
+}
---
+++ trunk/TerritoryManager/ConsoleClient/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleClient
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AddressSearch.AdressProvider;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameList = new List<string> { "anna", "michal", "tomasz", "jakub" };
+
+            var addressProvider = new KrakAddressProvider();
+
+            var list = addressProvider.getPersonList(2100, nameList);
+
+            foreach (var person in list)
+            {
+                Console.WriteLine("{0} {1}; Address: {2}, {3}", person.Name, person.Lastname, person.StreetAddress, person.PostCode);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
