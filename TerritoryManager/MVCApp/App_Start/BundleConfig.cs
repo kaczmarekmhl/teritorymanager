d3286353407723609b2dfe7dc8091851f2539b30
kaczmarek.mhl <kaczmarek.mhl@gmail.com>
2013-12-16 22:19:23 +0000

    Person selection in Ajax.

--- TerritoryManager/MVCApp/App_Start/BundleConfig.cs
+++ TerritoryManager/MVCApp/App_Start/BundleConfig.cs
@@ -13,7 +13,7 @@ namespace MVCApp
                 "~/Scripts/jquery-ui-{version}.js",
                 "~/Scripts/jquery.unobtrusive*",
                 "~/Scripts/jquery.validate*",
-                "~/Scripts/tmngr.js"
+                "~/Scripts/districtManager.js"
                 ));
             
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
---
+++ TerritoryManager/MVCApp/Scripts/districtManager.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+    $("input[ajax-selector='personSelectCheckbox']").change(function () {
+
+        var $input = $(this);
+
+        var data = {};
+        data['selected'] = $(this).is(':checked');
+        data['personId'] = $(this).attr('personId');
+        data['districtId'] = $(this).attr('districtId');
+              
+        $.ajax({
+            url: $(this).data('url'),
+            type: 'POST',
+            data: data,
+            success: function (result) {
+                $input.attr('checked', result.selected);
+            }
+        });
+    });
+});
