6c39ee01a19a01a2d3244ef201d833118f336b27
kaczmarek.mhl <kaczmarek.mhl@gmail.com>
2013-12-21 23:24:12 +0000

    Google Map + Save file

---
+++ TerritoryManager/KmlGenerator/Entities/Placemark.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KmlGenerator.Entities
+{
+    class Placemark
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+    }
+}
