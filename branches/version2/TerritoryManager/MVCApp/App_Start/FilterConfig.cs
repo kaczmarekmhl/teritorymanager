6392cd0d59b01e7d47ebf23a3c76383cc90fd507
kaczmarek.mhl@gmail.com <kaczmarek.mhl@gmail.com@bd7958f3-32f3-4e9c-53a0-a9d844338e45>
2014-01-01 15:16:27 +0000

    RequireHttps for release build.

--- branches/version2/TerritoryManager/MVCApp/App_Start/FilterConfig.cs
+++ branches/version2/TerritoryManager/MVCApp/App_Start/FilterConfig.cs
@@ -8,6 +8,9 @@ namespace MVCApp
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            #if !DEBUG
+                filters.Add(new RequireHttpsAttribute());
+            #endif
         }
     }
 }
