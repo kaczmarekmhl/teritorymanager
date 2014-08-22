00c2b99f8d2647cb6075ba13320037a32e20c324
kaczmarek.mhl <kaczmarek.mhl@gmail.com>
2014-08-22 10:04:59 +0000

    Missing file add

---
+++ TerritoryManager/AddressProvider/Filters/PersonFilter/Helpers/NameHelper.cs
@@ -0,0 +1,19 @@
+﻿using AddressSearch.AdressProvider.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AddressSearch.AdressProvider.Filters.PersonFilter.Helpers
+{
+    public class NameHelper
+    {
+        /// <summary>
+        /// Check if given person name is exact match to the search name
+        /// </summary>
+        public static bool isNameExact(Person person)
+        {
+            return person.Name.Contains(person.SearchName.Name);
+        }
+    }
+}
