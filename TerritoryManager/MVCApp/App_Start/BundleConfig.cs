b5dc737891bdf19fd291082bd4f12dc300079ed4
kaczmarek.mhl <kaczmarek.mhl@gmail.com>
2015-04-03 11:55:49 +0000

    Java script path fixes

--- TerritoryManager/MVCApp/App_Start/BundleConfig.cs
+++ TerritoryManager/MVCApp/App_Start/BundleConfig.cs
@@ -28,9 +28,6 @@ namespace MVCApp
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jquery.geocomplete").Include(
-                        "~/Scripts/jquery.geocomplete/jquery.geocomplete.js"));
-
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
