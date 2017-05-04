86b29cbcd1fa2b8a09d42ace910b581868522fe0
kaczmarekmhl <kaczmarek.mhl@gmail.com>
2017-05-04 23:44:47 +0200

    .Net Core AddressSearch service start.

---
+++ TerritoryManager/AddressSearchService/Controllers/SearchAddressController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AddressSearchService.Controllers
+{
+    [Route("api/[controller]")]
+    public class SearchAddressController : Controller
+    {
+        [HttpGet("{country}/{searchPhrases}")]
+        public IEnumerable<string> Search(string country, List<string> searchPhrases)
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
